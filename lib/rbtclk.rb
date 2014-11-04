fcdbb3f7cd1ddf7076d75e844bd35662260cc35c
Moza USANE <mozamimy@quellencode.org>
2014-11-04 11:07:48 +0900

    Implemented a function that shows version of rbtclk.

--- lib/rbtclk.rb
+++ lib/rbtclk.rb
@@ -12,10 +12,20 @@ module Rbtclk
         opt.on("--mode MODE") { |m| params[:mode] = m }
         opt.on("--format FORMAT") { |f| params[:format] = f }
         opt.on("--font FONT") { |f| params[:font] = f }
+        opt.on("--version") { |v| params[:version] = v }
 
         opt.parse!(args)
       end
 
+      if params[:version]
+        puts Rbtclk::VERSION
+        exit
+      else
+        display_clock(params)
+      end
+    end
+
+    def display_clock(params)
       case params[:mode] || "clock"
       when "clock"
         timer = Clock.new(font: params[:font] || "clb8x8", format: params[:format] || "%X")
