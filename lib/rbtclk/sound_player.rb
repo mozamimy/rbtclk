c1465118837c2b808d523c7c9642e88640f874ef
Moza USANE <mozamimy@quellencode.org>
2015-02-03 16:26:47 +0900

    Modified to use mpg123 instead of afplay.

--- lib/rbtclk/sound_player.rb
+++ lib/rbtclk/sound_player.rb
@@ -3,18 +3,30 @@
 # This software is released under the MIT License.
 # http://opensource.org/licenses/mit-license.php
 
+require "open3"
+
 module Rbtclk
   class SoundPlayer
     def initialize
-      # TODO: We need recognizing the OS to know which command we can use to play a sound file.
       sound_file_path = File.expand_path("../../../sound/bell.mp3", __FILE__)
-      @command_line = "afplay #{sound_file_path}"
+
+      @command_line = if command_exist?("afplay")
+                        "afplay #{sound_file_path}"
+                      elsif command_exist?("mpg123")
+                        "mpg123 -q #{sound_file_path}"
+                      else
+                        ":" # do noghing
+                      end
     end
 
-    # NOTE: This method is experimental.
-    #       It can be executed on only OS X.
     def play
       spawn @command_line
+    end
+
+    private
+
+    def command_exist?(command)
+      Open3.capture3("which #{command}")[2].success?
     end
   end
 end
