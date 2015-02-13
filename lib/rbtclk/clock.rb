3083f9303d1f80c67c2c5099d5145a70bb3e972f
Moza USANE <mozamimy@quellencode.org>
2015-02-13 16:42:01 +0900

    Refactor Clock

--- lib/rbtclk/clock.rb
+++ lib/rbtclk/clock.rb
@@ -18,12 +18,6 @@ module Rbtclk
     end
 
     def show
-      Curses.init_screen
-      Curses.start_color
-      Curses.use_default_colors
-      Curses.init_pair(1, translate(@color), -1)
-      Curses.curs_set(0)
-
       begin
         view_thread = Thread.new do
           loop do
@@ -50,6 +44,14 @@ module Rbtclk
     end
 
     private
+
+    def initialize_screen
+      Curses.init_screen
+      Curses.start_color
+      Curses.use_default_colors
+      Curses.init_pair(1, translate(@color), -1)
+      Curses.curs_set(0)
+    end
 
     def refresh
       Curses.clear
